5f55b444eeae740a9e35612b26e6b6a3e4252bf4
adityawintech <aditya#19AI>
2025-07-06 14:14:38 +0530

    Aditya

---
+++ Ch-10/python.py
@@ -0,0 +1,23 @@
+# Ternary Operaor 
+
+result="Adult" if 30>=20 else "Minor"
+# print(result)
+
+
+def printGrade(marks):
+    # Write your code here using a switch statement to print the grade based on marks
+    if marks >= 90 : print("Grade:A")
+    elif marks >= 80 and marks<=89 : print("Grade:B")
+    elif marks>=70 and marks<=79 : print("Grade:C")
+    elif marks>=60 and marks<=69 : print("Grade:D")
+    else : print("Grade:E")
+
+
+# score=int(input())
+# printGrade(score)
+
+
+# i=1  Starting 
+# while i<=10:  Keyword/Stoping   
+#     print(i) logic 
+#     i+=1   Beheviour
